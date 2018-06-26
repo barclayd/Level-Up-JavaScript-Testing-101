9e60a63eb40b23677668830269a511c77991eeec
Daniel Barclay <barclaysd@me.com>
2018-06-26 14:28:12 +0100

    Added multiple test cases for input types

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,24 @@
-import { hello } from './App';
+import { hello, add } from './App';
 
 describe('hello', () => {
     it('should output hello', () => {
         expect(hello()).toBe('Hello');
+    });
+});
+
+describe ('add', () => {
+    it('should add 2 numbers', () => {
+        expect(add(1, 2)).toBe(3);
+        expect(add(3, 4)).toBe(7);
+        expect(add(21, 4)).toBe(25);
+    });
+    it('should not add strings', () => {
+        expect(add(2,'2')).toBe(null);
+    });
+    it('should not add objects', () => {
+        expect(add(2,{})).toBe(null);
+    });
+    it('should not add arrays', () => {
+        expect(add(2,[])).toBe(null);
     });
 });
